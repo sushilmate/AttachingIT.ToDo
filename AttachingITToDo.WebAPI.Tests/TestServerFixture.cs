63dce35ceddf1c49040262cc7dc10e85f92e0c0b
sushilmate <sushil.mate@outlook.com>
2018-11-14 19:56:11 +0530

    Adding new test project in the solution.removing unused project code.

---
+++ AttachingITToDo.WebAPI.Tests/TestServerFixture.cs
@@ -0,0 +1,39 @@
+﻿using AttachingITToDo.WebUI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.PlatformAbstractions;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace AttachingITToDo.WebAPI.Tests
+{
+    public class TestServerFixture : IDisposable
+    {
+        private readonly TestServer _testServer;
+        public HttpClient Client { get; }
+
+        public TestServerFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseContentRoot(GetContentRootPath())
+                .UseEnvironment("Testing")
+                .UseStartup<Startup>();  // Uses Start up class from your API Host project to configure the test server
+
+            _testServer = new TestServer(builder);
+            Client = _testServer.CreateClient();
+        }
+
+        private string GetContentRootPath()
+        {
+            var integrationTestsPath = PlatformServices.Default.Application.ApplicationBasePath;
+            return Path.GetFullPath(Path.Combine(integrationTestsPath, "../../../../AttachingIT.ToDo"));
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _testServer.Dispose();
+        }
+    }
+}
---
+++ AttachingITToDo.WebAPI.Tests/ToDoDataController.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AttachingITToDo.WebAPI.Tests
+{
+    [TestClass]
+    public class ToDoDataController : TestServerFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
